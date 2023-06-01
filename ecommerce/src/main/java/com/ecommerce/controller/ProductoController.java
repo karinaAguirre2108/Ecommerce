05e4b6462d44db0f9dbbad75ed560f97745f5759
karinaAguirre2108 <aguirremendezkarina2108@gmail.com>
2023-05-31 23:26:05 -0500

    producto
    creacion de controlller,repository,service y template

--- ecommerce/src/main/java/com/ecommerce/controller/ProductoController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/ProductoController.java
@@ -1,5 +1,15 @@
 package com.ecommerce.controller;
 
-public class ProductoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/producto")
+public class ProductoController {
+	
+	@GetMapping("")
+	public String listar() {
+		return "producto/listar";
+	}
 }
