62fc692d37f92c2d4c70ff2d16684b557ffc3154
karinaAguirre2108 <aguirremendezkarina2108@gmail.com>
2023-07-02 21:16:37 -0500

    home-version1
    se realizo la prueba del slider y se itera el card de productos con la primera imagen asociada

--- ecommerce/src/main/java/com/ecommerce/controller/AdminstradorController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/AdminstradorController.java
@@ -1,15 +1,31 @@
 package com.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.model.Categoria;
+import com.ecommerce.model.Imagen;
+import com.ecommerce.model.Producto;
+import com.ecommerce.service.ICategoriaService;
+import com.ecommerce.service.IProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdminstradorController {
 	
+	@Autowired
+	private IProductoService productoService;
+	
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		List<Producto> productos=productoService.getAllProducto();
+		model.addAttribute("productos", productos);
 		return "administrador/home";
 	}
 
